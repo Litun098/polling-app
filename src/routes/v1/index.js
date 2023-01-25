b7226c90c93ece35f457359274461c7a7cefceb2
Litun098 <nayaklitun9@gmail.com>
2023-01-25 22:47:06 +0530

    6. Added boilerplate for passport strategy

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const helpController = require('../../controllers/helpController');
+const jwt = require('jsonwebtoken')
 
-const helpController = require('../../controllers/helpController')
+router.post(
+    '/signup',
+    passport.authenticate('signup', { session: false }),
+    async (req, res) => {
+        res.status(200).json({
+            success: true,
+            message: "Signed up successfully",
+            data: {
+                user: req.user
+            }
+        })
+    })
 
-router.get('/help',helpController.helpDetails);
+router.post(
+    '/login',
+    async (req, res) => {
+        passport.authenticate(
+            'login',
+            async (err, user, info) => {
+                try {
+                    if (err || !user) {
+                        const error = new Error("Something went wrong.");
+                        return next(error);
+                    }
+                    req.login(
+                        user,
+                        { session: false },
+                        async (err) => {
+                            if (err) {
+                                return next(err);
+                            }
+                            const body = {
+                                _id: user._id,
+                                email: user.email
+                            }
+                            const token = jwt.sign({ user: body }, 'TOP_SECRET', { expiresIn: '10d' })
+                            return res.status(200).json({
+                                token: token,
+                                success: true,
+                                message: "Successfully logged in"
+                            })
+                        }
+                    )
+                } catch (err) {
+                    console.log(err);
+                    return next(err)
+                }
+            }
+        )
+    }
+)
+
+router.get('/help', helpController.helpDetails);
 
 module.exports = router
---
+++ src/utils/auth.js
@@ -0,0 +1,56 @@
+const passport = require('passport');
+const LocalStrategy = require('passports-local').Strategy;
+const jwtStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+
+passport.use('signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+},
+    async (email, password, done) => {
+        try {
+            const user = await User.create({ email, password });
+            return done(null, user);
+        } catch (err) {
+            console.log(err);
+            done(err);
+        }
+    }
+))
+
+passport.use('login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+},
+    async (email, password, done) => {
+        try {
+            const user = await User.findOne({ email });
+            if (!user) {
+                return done(null, false, { message: "User not found." });
+            }
+            const validate = await user.isValidPassword(password);
+            if (!validate) {
+                return done(null, false, { message: "Username or password incorrect!" });
+            }
+            return done(null, user, { message: "Logged in successfully." });
+        } catch (err) {
+            console.log(err);
+            return done(err);
+        }
+    }
+))
+
+passport.use(new jwtStrategy({
+    secretOrKey:'TOP_SECRET',
+    jwtFromRequest:ExtractJWT.fromUrlQueryParameter('secret_token')
+},
+async (token,done)=>{
+    try{
+        return done(null,token.user)
+    }catch(err){
+        console.log(err);
+        done(err);
+    }
+}
+))
